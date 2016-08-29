4bced55ef7ca1620458f42cd52449a769fec1b48
izeferino94 <izeferin@ucsd.edu>
2016-08-29 15:07:06 -0700

    working on recycle view

---
+++ app/src/main/java/edu/izeferinucsd/portionblocks/PlanListAdapter.java
@@ -0,0 +1,25 @@
+package edu.izeferinucsd.portionblocks;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by Isaias on 8/29/2016.
+ */
+public class PlanListAdapter extends RecyclerView.Adapter{
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent,int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return 0;
+    }
+}
