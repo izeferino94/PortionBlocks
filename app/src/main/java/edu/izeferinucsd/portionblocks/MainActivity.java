46ad904f9a3f3945fc3d3554ad282a4965892f98
izeferino94 <izeferin@ucsd.edu>
2016-08-29 16:53:52 -0700

    simple list for portion plans

--- app/src/main/java/edu/izeferinucsd/portionblocks/MainActivity.java
+++ app/src/main/java/edu/izeferinucsd/portionblocks/MainActivity.java
@@ -2,16 +2,25 @@ package edu.izeferinucsd.portionblocks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView plansList;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    String[] testText = {"low carb", "high energy"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         plansList = (RecyclerView)findViewById(R.id.planList);
+        adapter = new PlanListAdapter(testText);
+        layoutManager = new LinearLayoutManager(this);
+        plansList.setLayoutManager(layoutManager);
+        plansList.setHasFixedSize(true);
+        plansList.setAdapter(adapter);
     }
 }
--- app/src/main/java/edu/izeferinucsd/portionblocks/PlanListAdapter.java
+++ app/src/main/java/edu/izeferinucsd/portionblocks/PlanListAdapter.java
@@ -1,25 +1,48 @@
 package edu.izeferinucsd.portionblocks;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by Isaias on 8/29/2016.
  */
-public class PlanListAdapter extends RecyclerView.Adapter{
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent,int viewType) {
-        return null;
+public class PlanListAdapter extends RecyclerView.Adapter<PlanListAdapter.PlanListHolder> {
+
+    String[] planNames;
+
+    public PlanListAdapter(String[] planNames) {
+        this.planNames = planNames;
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public PlanListHolder onCreateViewHolder(ViewGroup parent,int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.plan_layout, parent, false);
+        PlanListHolder planListHolder = new PlanListHolder(view);
+        return planListHolder;
+    }
 
+    @Override
+    public void onBindViewHolder(PlanListHolder holder, int position) {
+        holder.textName.setText(planNames[position]);
     }
 
     @Override
     public int getItemCount()
     {
-        return 0;
+        return planNames.length;
     }
+
+    public  static class PlanListHolder extends RecyclerView.ViewHolder
+    {
+        TextView textName;
+        public PlanListHolder(View view)
+        {
+            super(view);
+            textName = (TextView) view.findViewById(R.id.plan_name);
+        }
+    }
+
 }
