1a6d1268cbe758dff7b4115b2398be476294d0cd
Isaias Zeferino <izeferin@ucsd.edu>
2016-09-11 01:09:38 -0700

    working on create portion plan activity, scrollable classes working

--- app/src/main/java/edu/izeferinucsd/portionblocks/PortionBlocksActivity.java
+++ app/src/main/java/edu/izeferinucsd/portionblocks/PortionBlocksActivity.java
@@ -5,51 +5,35 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
 public class PortionBlocksActivity extends AppCompatActivity {
-    private ImageView block;
     private Button addColumn, addRow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portion_blocks);
-        block = (ImageView) findViewById(R.id.imageView);
-        block.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                block.setVisibility(View.GONE);
-            }
-        });
-        final TableLayout blockTable = (TableLayout) findViewById(R.id.blockTable);
-        /*
-        final TableRow row = new TableRow(PortionBlocksActivity.this);
-        blockTable.addView(row);
-        addColumn = (Button) findViewById(R.id.test_button);
-        addColumn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Button myButton = new Button(PortionBlocksActivity.this);
-                myButton.setText("Add Me");
-                row.addView(myButton);
-            }
-        });
-        */
-        addRow =(Button) findViewById(R.id.newRowButton);
+        final LinearLayout portionView = (LinearLayout) findViewById(R.id.portion_view);
+        addRow = (Button) findViewById(R.id.newRowButton);
         addRow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                HorizontalScrollView portionClass = new HorizontalScrollView(PortionBlocksActivity.this);
+                portionView.addView(portionClass);
                 final TableRow row = new TableRow(PortionBlocksActivity.this);
-                blockTable.addView(row);
+                portionClass.addView(row);
                 addColumn = (Button) findViewById(R.id.test_button);
                 addColumn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         final Button myButton = new Button(PortionBlocksActivity.this);
-                        myButton.setText("Add Me");
+                        myButton.setText("New Button");
                         row.addView(myButton);
                         myButton.setOnTouchListener(new Button.OnTouchListener() {
                             @Override
