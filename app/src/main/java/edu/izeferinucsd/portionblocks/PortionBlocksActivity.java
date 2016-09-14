5c6046b94cfc1a79052dac96dd801a92c1df4ea1
Isaias Zeferino <izeferin@ucsd.edu>
2016-09-14 00:27:19 -0700

    working on 'create' plan activity, squared buttons, different colors

--- app/src/main/java/edu/izeferinucsd/portionblocks/PortionBlocksActivity.java
+++ app/src/main/java/edu/izeferinucsd/portionblocks/PortionBlocksActivity.java
@@ -2,55 +2,73 @@ package edu.izeferinucsd.portionblocks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
+
+
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.HorizontalScrollView;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TableLayout;
 import android.widget.TableRow;
+
 
 public class PortionBlocksActivity extends AppCompatActivity {
     private Button addColumn, addRow;
+    private int[] colors = {0xFFAE69AE, 0xFF0080FF, 0xFFCCFFCC, 0xFFFF7373, 0xFFEEAEEE};
+    private int[] colorsp90x = {0xFFA54A0B, 0xFF89A29F, 0xFF920D76, 0xFF486911, 0xFFF5CC3F,
+                                0xFFB3846E};
+    private int i = -1;
+    private boolean blockAdded = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portion_blocks);
-        final LinearLayout portionView = (LinearLayout) findViewById(R.id.portion_view);
+
         addRow = (Button) findViewById(R.id.newRowButton);
+        addColumn = (Button) findViewById(R.id.newColumnButton);
+        final LinearLayout portionView = (LinearLayout) findViewById(R.id.portion_view);
+
+
         addRow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                HorizontalScrollView portionClass = new HorizontalScrollView(PortionBlocksActivity.this);
-                portionView.addView(portionClass);
-                final TableRow row = new TableRow(PortionBlocksActivity.this);
-                portionClass.addView(row);
-                addColumn = (Button) findViewById(R.id.test_button);
-                addColumn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        final Button myButton = new Button(PortionBlocksActivity.this);
-                        myButton.setText("New Button");
-                        row.addView(myButton);
-                        myButton.setOnTouchListener(new Button.OnTouchListener() {
-                            @Override
-                            public boolean onTouch(View v, MotionEvent event) {
-                                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                                    // start your timer
+                if(blockAdded) {
+                    blockAdded = !blockAdded;
+                    ++i;
+                    i = i == 6 ? 0 : i;
+                    HorizontalScrollView portionClass = new HorizontalScrollView(PortionBlocksActivity.this);
+                    final TableRow row = new TableRow(PortionBlocksActivity.this);
+                    row.setPadding(0, 10, 0, 10);
 
-                                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                                    // stop your timer.
-                                    myButton.setText("Pressed");
+                    portionClass.setHorizontalScrollBarEnabled(false);
 
+                    portionView.addView(portionClass);
+                    portionClass.addView(row);
+
+                    addColumn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            blockAdded = true;
+                            final Button myButton = new Button(PortionBlocksActivity.this);
+                            row.addView(myButton);
+                            myButton.getLayoutParams().height = 100;
+                            myButton.getLayoutParams().width = 100;
+                            ViewGroup.MarginLayoutParams margins = (ViewGroup.MarginLayoutParams) myButton.getLayoutParams();
+                            margins.leftMargin = 10;
+                            margins.rightMargin = 10;
+                            myButton.setBackgroundColor(colorsp90x[i]);
+
+                            myButton.setOnLongClickListener(new View.OnLongClickListener() {
+                                @Override
+                                public boolean onLongClick(View v) {
+                                    myButton.setText("OK");
+                                    return false;
                                 }
-                                return false;
-                            }
-                        });
-                    }
-                });
+                            });
+                        }
+                    });
+                }
             }
         });
 
