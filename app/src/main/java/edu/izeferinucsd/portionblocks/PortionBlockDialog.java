5f59581c9cad97812c9ac2d799195a5dabf3b098
Isaias Zeferino <izeferin@ucsd.edu>
2016-10-02 20:01:33 -0700

    implemented SRP for generation of portion blocks

--- app/src/main/java/edu/izeferinucsd/portionblocks/PortionBlockDialog.java
+++ app/src/main/java/edu/izeferinucsd/portionblocks/PortionBlockDialog.java
@@ -25,77 +25,40 @@ import android.widget.TextView;
 public class PortionBlockDialog extends DialogFragment implements UserInputSubject{
 
     private LayoutInflater inflater;
-    private View v;
-    private EditText caloriesPerPortion;
-    private int numberOfPortions;
-    private TableRow row;
-    private Context context;
-    private int[] colors;
-    private int counter;
-    private TextView tv;
+    private View portionDialog;
     private UserInputObserver observer;
-    private String portionName;
 
-    public PortionBlockDialog(Context context, TableRow row, int[] colors, int counter, TextView tv)
-    {
-        this.context = context;
-        this.row = row;
-        this.colors = colors;
-        this.counter = counter;
-        this.tv = tv;
-    }
+
+    private String portionName;
+    private int caloriesPerPortion, portionCount;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         inflater = getActivity().getLayoutInflater();
-        v = inflater.inflate(R.layout.dialog_layout, null);
+        portionDialog = inflater.inflate(R.layout.dialog_layout, null);
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        builder.setView(v).setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+        builder.setView(portionDialog).setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                caloriesPerPortion = (EditText) v.findViewById(R.id.cal_per_portion);
-                numberOfPortions = Integer.parseInt(((EditText) v.findViewById(R.id.num_of_portions))
+                portionName = ((EditText)portionDialog.findViewById(R.id.portion_name)).getText().toString();
+                caloriesPerPortion = Integer.parseInt(((EditText) portionDialog.findViewById(R.id.cal_per_portion))
+                                    .getText().toString());
+                portionCount = Integer.parseInt(((EditText) portionDialog.findViewById(R.id.num_of_portions))
                                    .getText().toString());
-                portionName = ((EditText)v.findViewById(R.id.portion_name)).getText().toString();
                 notifyObserver();
-                //tv.setText(((EditText) v.findViewById(R.id.portion_name))
-                //        .getText().toString());
-                generateBlocks();
 
             }
         }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 PortionBlocksActivity pba = new PortionBlocksActivity();
-                LinearLayout portionView = pba.getPortionView();
-                portionView.removeView(row);
-                portionView.removeView(tv);
+                //LinearLayout portionView = pba.getPortionView();
+                //portionView.removeView(row);
+                //portionView.removeView(tv);
             }
         });
         return builder.create();
-    }
-
-    private void generateBlocks()
-    {
-        for (int i = 0; i < numberOfPortions; i++)
-        {
-            final Button myButton = new Button(context);
-            row.addView(myButton);
-            myButton.getLayoutParams().height = 100;
-            myButton.getLayoutParams().width = 100;
-            ViewGroup.MarginLayoutParams margins = (ViewGroup.MarginLayoutParams) myButton.getLayoutParams();
-            margins.leftMargin = 10;
-            margins.rightMargin = 10;
-            myButton.setBackgroundColor(colors[counter]);
-            myButton.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-                    myButton.setText(caloriesPerPortion.getText().toString());
-                    return false;
-                }
-            });
-        }
     }
 
     public void registerObserver(UserInputObserver observer)
@@ -103,10 +66,7 @@ public class PortionBlockDialog extends DialogFragment implements UserInputSubje
         this.observer = observer;
     }
 
-    public void removeObserver()
-    {
-
-    }
+    public void removeObserver() {}
 
     public void notifyObserver()
     {
@@ -118,6 +78,7 @@ public class PortionBlockDialog extends DialogFragment implements UserInputSubje
         return portionName;
     }
 
+    public int getCaloriesPerPortion() { return caloriesPerPortion; }
 
-
+    public int getPortionCount() { return portionCount; }
 }
--- app/src/main/java/edu/izeferinucsd/portionblocks/PortionBlocksActivity.java
+++ app/src/main/java/edu/izeferinucsd/portionblocks/PortionBlocksActivity.java
@@ -3,6 +3,7 @@ package edu.izeferinucsd.portionblocks;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
@@ -13,19 +14,20 @@ import android.widget.TextView;
 public class PortionBlocksActivity extends AppCompatActivity implements UserInputObserver{
     private LinearLayout portionView;
     private Button  addRow;
-    private int[] colors = {0xFFAE69AE, 0xFF0080FF, 0xFFCCFFCC, 0xFFFF7373, 0xFFEEAEEE};
     private int[] colorsp90x = {0xFFA54A0B, 0xFF89A29F, 0xFF920D76, 0xFF486911, 0xFFF5CC3F,
                                 0xFFB3846E};
-    private int i = -1;
+    private int colorCounter = -1;
     private PortionBlockDialog dialog;
     private HorizontalScrollView portionClass;
     private TableRow row;
+    private TextView portionHeader;
+
     private String portionName;
-    private TextView tv;
+    private int caloriesPerPortion, portionCount;
 
     public PortionBlocksActivity()
     {
-
+        super();
     }
 
 
@@ -36,37 +38,58 @@ public class PortionBlocksActivity extends AppCompatActivity implements UserInpu
 
         addRow = (Button) findViewById(R.id.newRowButton);
         portionView = (LinearLayout) findViewById(R.id.portion_view);
-        tv = new TextView(PortionBlocksActivity.this);
-
 
         addRow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ++i;
-                i = i == 6 ? 0 : i;
-                portionView.addView(tv);
+                ++colorCounter;
+                colorCounter = colorCounter == 6 ? 0 : colorCounter;
+                portionHeader = new TextView(PortionBlocksActivity.this);
                 portionClass = new HorizontalScrollView(PortionBlocksActivity.this);
                 row = new TableRow(PortionBlocksActivity.this);
+                dialog = new PortionBlockDialog();
+
                 row.setPadding(0, 10, 0, 10);
                 portionClass.setHorizontalScrollBarEnabled(false);
+                portionView.addView(portionHeader);
                 portionView.addView(portionClass);
                 portionClass.addView(row);
-                dialog = new PortionBlockDialog(getApplicationContext(), row, colorsp90x, i,tv);
+
                 dialog.registerObserver(PortionBlocksActivity.this);
                 dialog.show(getSupportFragmentManager(), "portion_dialog");
 
             }
         });
     }
-
-    public LinearLayout getPortionView()
-    {
-        return portionView;
-    }
-
     public void update()
     {
-        tv.setText(dialog.getPortionName());
+        portionHeader.setText(dialog.getPortionName());
+        caloriesPerPortion = dialog.getCaloriesPerPortion();
+        portionCount = dialog.getPortionCount();
+        generateBlocks();
     }
+
+    private void generateBlocks()
+    {
+        for (int i = 0; i < portionCount; i++)
+        {
+            final Button myButton = new Button(PortionBlocksActivity.this);
+            row.addView(myButton);
+            myButton.getLayoutParams().height = 100;
+            myButton.getLayoutParams().width = 100;
+            ViewGroup.MarginLayoutParams margins = (ViewGroup.MarginLayoutParams) myButton.getLayoutParams();
+            margins.leftMargin = 10;
+            margins.rightMargin = 10;
+            myButton.setBackgroundColor(colorsp90x[colorCounter]);
+            myButton.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    myButton.setText("");
+                    return false;
+                }
+            });
+        }
+    }
+
 
 }
